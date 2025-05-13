9555b72c9b416c941b92cc92657d8a6543c16f8b
Analu557 <ana.luiza.pinheiro@escola.pr.gov.br>
2025-05-13 13:29:52 +0000

    fim

---
+++ ex.js
@@ -0,0 +1,47 @@
+
+let nome = "Ana";
+console.log(`Olá, meu nome é ${nome}.`);
+
+
+let idade = 17;
+let cidade = "Almirante Tamandaré";
+console.log(`Tenho ${idade} anos e moro em ${cidade}.`);
+
+
+let comida1 = "Pizza";
+let comida2 = "Chá";
+let comida3 = "batata";
+console.log(`Minhas comidas favoritas são: ${comida1}, ${comida2} e ${comida3}.`);
+
+let preco = 65.99;
+console.log(`O produto custa R$ ${preco}.`);
+
+
+let produto = "tenis";
+let quantidade = 3;
+console.log(`Comprei ${quantidade} unidades de ${produto}.`);
+
+
+let temDesconto = true;
+console.log(`Tem desconto disponível? ${temDesconto}`);
+
+
+let anoAtual = 2025;
+let anoNascimento = 2007;
+let idadeCalculada = anoAtual - anoNascimento;
+console.log(`Você tem ${idadeCalculada} anos.`);
+
+
+let curso = "Back-End";
+let instituicao = "Ambrosio Bini";
+console.log(`Estou cursando ${curso} no ${instituicao}.`);
+
+
+let cor1 = "azul";
+let cor2 = "verde";
+let cor3 = "vermelho";
+console.log(`Minhas cores preferidas são ${cor1}, ${cor2} e ${cor3}.`);
+
+
+let mensagem = "Mensagem de finalização.";
+console.log(mensagem);
